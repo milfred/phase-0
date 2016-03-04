6b204fc7b0046aed99113b72ad5574899a1a1d19
milfred <jasonmilfred@gmail.com>
2016-03-04 10:59:53 -0600

    Add variable-methods file with both finished mini-challenges from Chris Pine book

--- week-4/variables-methods.rb
+++ week-4/variables-methods.rb
@@ -1,3 +1,6 @@
+
+# Full name greeting mini-challenge
+
 puts "What\'s your first name?"
 first_name = gets.chomp
 puts "What\'s your middle name?"
@@ -5,3 +8,10 @@ middle_name = gets.chomp
 puts "What\'s your last name?"
 last_name = gets.chomp
 puts "Hello #{first_name} #{middle_name} #{last_name}!"
+
+# Bigger, better, favorite number mini-challenge
+
+puts "What\'s your favorite number?"
+favorite_num = gets.chomp.to_i + 1
+
+puts "That\'s nice, but don\'t you think #{favorite_num} would be better? It is bigger you know."
