1b6cabb90bb5c117ef7ac78353a1853b5895c6a0
milfred <jasonmilfred@gmail.com>
2016-03-04 15:27:04 -0600

    Update variable-methods.rb file to include my refleciton and links to exercise solution files

--- week-4/variables-methods.rb
+++ week-4/variables-methods.rb
@@ -14,3 +14,26 @@ puts 'Hello #{first_name} #{middle_name} #{last_name}!'
 puts 'What\'s your favorite number?'
 favorite_num = gets.chomp.to_i + 1
 puts 'That\'s nice, but don\'t you think #{favorite_num} would be better? It is bigger you know.'
+
+# How do you define a local variable?
+# You declare the variable name, type an equal sign, and then enter the variable (e.g. local_var = "the variable").
+
+# How do you define a method?
+# To define a method you type "def," then add a space, and then define the name of the method. If the method has arguements you would include them after the name. The arguements can be inside parentheses, but they don't have to be as long as there is a space after the name (e.g. def method(arguement1, arguement2)).
+
+# What is the difference between a local variable and a method?
+# A local variable stores information for later use by calling the name of the variable. A method is code that performs an action.
+
+# How do you run a ruby program from the command line?
+# To run a ruby program from the command line you simply type "ruby name_of_program.rb"
+
+# How do you run an RSpec file from the command line?
+# To run an RSpec file from the command line type "rspec name_of_file.rb"
+
+# What was confusing about this material? What made sense?
+# While I definitely had to do some refactoring, I think I ultimately grasped the material in the module.
+
+# Exercise Solution File Links
+# https://github.com/milfred/phase-0/blob/master/week-4/define-method/my_solution.rb
+# https://github.com/milfred/phase-0/blob/master/week-4/address/my_solution.rb
+# https://github.com/milfred/phase-0/blob/master/week-4/math/my_solution.rb
