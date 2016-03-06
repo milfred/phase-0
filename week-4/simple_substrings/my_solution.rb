d8f4e2dc6775f26a7fbcca5cb1e655f198daa8c8
milfred <jasonmilfred@gmail.com>
2016-03-06 16:18:56 -0600

    Update to simple substrings solution

--- week-4/simple_substrings/my_solution.rb
+++ week-4/simple_substrings/my_solution.rb
@@ -6,12 +6,13 @@
 # Your Solution Below
 
 def welcome(address)
-  if address.include?('CA') == true
+  if address.include?(' CA') == true
     return 'Welcome to California'
   else
     return 'You should move to California'
   end
 end
+
 
 puts welcome('123 Highbrow St., San Francisco, CA 99999')
 puts welcome('123 Sesame St. New York, NY 11111')
