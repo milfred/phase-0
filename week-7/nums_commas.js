a1e120a9b80c87005056251d9b9a484a3301454e
milfred <jasonmilfred@gmail.com>
2016-03-26 14:35:03 -0500

    Add completed solution file for module 7.5

--- week-7/nums_commas.js
+++ week-7/nums_commas.js
@@ -7,7 +7,7 @@
 // Input: A integer
 // Output: A string with commas separating the number every three digits
 // Steps:
-// 1. // Separate Numbers with Commas in JavaScript **Pairing Challenge**
+// 1. Separate Numbers with Commas in JavaScript
 // 2. Transform number into string
 // 3. Reverse the string
 // 4. If the length of the string is less than 4, print the string
@@ -31,48 +31,40 @@
 //       arrayReverse.splice(arrayReverse[i], 0, ",");
 //       return arrayReverse;
 //     }
-//   // var counter = 3;
-//   // for(var i=0; i < counter; i++)
+//   var counter = 3;
+//   for(var i=0; i < counter; i++)
 
 // }
-// console.log(separateComma(123456789123))
-
-
 
 // Refactored Solution
 
 function commaSeparator(number) {
-  var reversedArray = number.toString().split("").reverse()
-  return reversedArray
-
+  var reversedArray = number.toString().split("").reverse();
+  var i = 3;
+  do {
+    reversedArray.splice(i, 0, ",");
+    i += 4;
+  } while (i < reversedArray.length);
+  return reversedArray.reverse().join("");
 }
 
-
-
-
-console.log(separateComma(123456789123))
-
 // Your Own Tests (OPTIONAL)
 
-
-
+console.log(commaSeparator(123456789123))
+console.log(commaSeparator(1234567891231235568))
+console.log(commaSeparator(12345678912312355680))
 
 // Reflection
 
+// What was it like to approach the problem from the perspective of JavaScript? Did you approach the problem differently?
+// The approach was pretty similar to what I did in Ruby. The JavaScript, while more verbose than my Ruby solution, is probably a bit more readable. In Ruby I just chained a bunch of methods together. It was one line, but a very long line. In JavaScript I had to break it up into more pieces that make it easier to tell what is going on.
 
-// Initial Solution
+// What did you learn about iterating over arrays in JavaScript?
+// I learned the "do...while" method which I was not familiar with, but is basically the same as "while" in Ruby.
 
+// What was different about solving this problem in JavaScript?
+// I wish I could say that it was easier to do the same exercise in JavaScript because of the knowledge I have from Ruby, but it took me quite awhile because I'm still trying to ingrain the syntax.
 
+// What built-in methods did you find to incorporate in your refactored solution?
+// I used .toString(), .split(), .reverse(), .splice(), .length, .do...while, and .join().
 
-
-// Refactored Solution
-
-
-
-
-// Your Own Tests (OPTIONAL)
-
-
-
-
-// Reflection
