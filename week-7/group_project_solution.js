0e7e5abf76c90ea0be90ac6121445dfcff547b3c
milfred <jasonmilfred@gmail.com>
2016-03-26 19:22:11 -0500

    Remove driver code from solution file

--- week-7/group_project_solution.js
+++ week-7/group_project_solution.js
@@ -1,24 +1,24 @@
+// What I think the user stories are
+
+// 1. As a user, I want to be able to input a group of numbers into a function called "sum" and have it give me the sum of all numbers in the group.
+// 2. As a user, I want to be able to input a group of numbers into a function called "mean" and have it give me the mean of those numbers.
+// 3. As a user, I want to be able to input a group of numbers into a function called "median" and have it give me the median of those numbers. If there are an even number of values in the group of numbers, I also want the function to give me the two numbers that are divided to produce the mean.
+
 // Refactored code
 
-var numArray = [1,2,3]
-
-// Create function to sum numbers in an array
+// Function to sum numbers in an array
 function sum(numbers) {
   return numbers.reduce(function(a, b) {
     return a + b;
   });
 }
 
-console.log(sum(numArray));
-
-// Create function to return the mean of the sum of all numbers in an array
+// Function to return the mean of the sum of all numbers in an array
 function mean(numbers) {
   return sum(numbers) / numbers.length;
 }
 
-console.log(mean([1,2,3]));
-
-// Create a function to find the median of a set of numbers in an array
+// Function to find the median of a set of numbers in an array
 // If the array has an even number of values, return the median and an array containing the two digits that will be divided to get the median
 function median(number) {
   number.sort();
@@ -34,11 +34,3 @@ function median(number) {
     return (addedSlice[0] + addedSlice[1]) / 2;
     }
 }
-
-console.log(median([1,2,3]));
-console.log(median([1,2,3,4]));
-
-// User stories
-// 1. As a user, I want to be able to input a group of numbers into a function called "sum" and have it give me the sum of all numbers in the group.
-// 2. As a user, I want to be able to input a group of numbers into a function called "mean" and have it give me the mean of those numbers.
-// 3. As a user, I want to be able to input a group of numbers into a function called "median" and have it give me the median of those numbers. If there are an even number of values in the group of numbers, I also want the function to give me the two numbers that are divided to product the mean.
